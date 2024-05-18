ef1c70010ae5ce1a83efb1a6ef8ed68c9892d4a6
Jacob <jacobphelan@hotmail.com>
2024-05-19 00:02:28 +1000

    add: feature Link#New fix #10

--- spec/features/link/index_spec.rb
+++ spec/features/link/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Link Index" do
 		link1 = Link.create!(shortlink: "1a", link: "http://instagram.com")
 		link2 = Link.create!(shortlink: "be", link: "http://kogan.com")
 
-		visit "/"
+		visit "/links"
 
 		expect(page).to have_content("Shortlink: http://www.example.com/#{link1.shortlink}")
 	end
@@ -13,7 +13,7 @@ RSpec.describe "Link Index" do
 		link1 = Link.create!(shortlink: "1a", link: "http://instagram.com")
 		link2 = Link.create!(shortlink: "be", link: "http://kogan.com")
 
-		visit "/"
+		visit "/links"
 
 		expect(page).to have_content("Link: #{link1.link}")
 	end
