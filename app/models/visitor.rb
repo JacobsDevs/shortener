f152ca2169738786b9eb317207943ea4c6cebd3c
Jacob <jacobphelan@hotmail.com>
2024-05-27 22:54:50 +1000

    attempting to fix location in deployment #7

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -4,14 +4,13 @@ class Visitor < ApplicationRecord
 	has_many :links_visitors
 	has_many :links, through: :links_visitors
 
-	after_commit :init
+	after_create :init
 	
 	private
 	def init
 		results = Geocoder.search(ip_address)
-		puts(results.first.country)
-		puts(results.first.city)
-		self.update!(country: results.first.country)
+
+		self.country = results.first.country
 		self.update!(city: results.first.city)
 	end
 end
