3ccff5021c0f33c4968186c2417d6db472541603
Jacob <jacobphelan@hotmail.com>
2024-05-28 23:09:41 +1000

    proto: Link#index & layout basic styling

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+		'./app/views/layouts/*.{erb,haml,html,slim}',
+		'./app/views/link/*.{erb,haml,html,slim}',
+		'./app/views/visitor/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
@@ -18,5 +21,13 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
+  ],
+	safelist: [
+		'rounded-md', 
+		'shadow-md',
+		'shadow-xl',
+		'w-fit',
+		'ring-2',
+		'ring-slate-600'
+	]
 }
