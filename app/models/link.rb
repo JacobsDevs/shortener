baf8d353ac8118a7e0730c4776e193b0ece5a101
Jacob <jacobphelan@hotmail.com>
2024-05-30 22:18:32 +1000

    add: link_to for links

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,11 +4,7 @@ class Link < ApplicationRecord
 	validates :shortlink, :link, presence: true
 
 	def log_visit(ip_address)
-		if self.visit_count == nil
-			self.visit_count = 1
-		else
-			self.visit_count = visit_count + 1
-		end
+		self.visit_count ? self.visit_count+=1 : self.visit_count = 1
 		self.visitors.create!(ip_address: ip_address)
 		self.save
 	end
