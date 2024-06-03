12b9de2ad711197872e1cb5173e5bbf1d3a300d1
Jacob <jacobphelan@hotmail.com>
2024-06-04 00:26:22 +1000

    add: 1st iteration of Repeat Visits

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,14 +6,12 @@ class Link < ApplicationRecord
 	def log_visit(ip_address)
 		self.visit_count ? self.visit_count+=1 : self.visit_count = 1
 		if self.visitors.where(ip_address: ip_address).size == 0
-			a = self.visitors.create!(ip_address: ip_address)
-			self.links_visitors.find_by(visitor_id: a.id).update!(visits: 1)
-		else
-			a = self.visitors.find_by(ip_address: ip_address)
-			b = self.links_visitors.find_by(visitor_id: a.id)
-			b.increment(:visits)
-			b.save
+			self.visitors.create!(ip_address: ip_address)
+			self.save
 		end
-		self.save
+		a = self.visitors.find_by(ip_address: ip_address)
+		b = self.links_visitors.find_by(visitor_id: a.id)
+		b.increment!(:visits)
+		b.save
 	end
 end
