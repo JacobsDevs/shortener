0686c97b8f7cd1ad4248856e653600293b44dd0c
Jacob <jacobphelan@hotmail.com>
2024-06-19 15:11:30 +1000

    add: LandingPage#save_fields(params) accepts params that it will then iterate through and update the field's Labels, more will updateable options to be added

--- app/models/landing_page.rb
+++ app/models/landing_page.rb
@@ -4,4 +4,10 @@ class LandingPage < ApplicationRecord
 	def add_text_field
 		fields.create!(name: "new_field_name", field_type: "text", label: "label")
 	end
+
+	def save_fields(params)
+		fields.each do |field|
+			field.update!(label: params["field_#{field.id}_label"])
+		end
+	end
 end
--- spec/models/landing_page_spec.rb
+++ spec/models/landing_page_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe LandingPage, type: :model do
 	it 'exists' do
 		landing_page = LandingPage.create!()
 	end
+	it 'can save progress' do
+		landing_page = LandingPage.create!()
+		landing_page.add_text_field
+		expect(landing_page.fields.last.label).to eq("label")
+		params = {
+							"field_#{landing_page.fields.last.id}_label" => "New Label",
+		}
+		landing_page.save_fields(params)
+		expect(landing_page.fields.last.label).to eq("New Label")
+	end
 end
