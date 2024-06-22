2bfc69ec9a07c235460ff3527581b54712d0093a
Jacob <jacobphelan@hotmail.com>
2024-06-22 23:43:50 +1000

    fix: error with fields not displaying after adding partials it didn't display the value when you did have a value in there.

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -2,14 +2,18 @@ class CustomerController < ApplicationController
 	def show
 		@customer = Customer.find(params[:id])
 	end
+
 	def new
 	end
+	
 	def create
 		customer = Customer.create!(customer_params)
 
 		redirect_to "/customers/#{customer.id}"
 	end
+	
 	private
+	
 	def customer_params
 		params.permit(:name, :email, :phone_number)
 	end
